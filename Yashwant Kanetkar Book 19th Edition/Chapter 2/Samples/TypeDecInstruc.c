29fcf3b2e00cb5f55e93dc14abe885a7fd9edd72
shubhamgharage-crenvotech <shubhamgharage@crenvotech.com>
2025-02-06 16:58:51 +0530

    Instruction: Type Declaration

---
+++ Yashwant Kanetkar Book 19th Edition/Chapter 2/Samples/TypeDecInstruc.c
@@ -0,0 +1,21 @@
+/* Type Declaration Instructions */
+/*
+Author: Shubham
+Date: 02/02/2025
+*/
+
+/*
+    Thre are 3 main types of Instructions in C:
+    1. Type declaration Instructions
+    2. Arithmatic Instructions
+    3. Control Instructions
+*/
+#include<stdio.h>
+
+int main()
+{
+    int a, b, c;
+    float f, gr;
+    char ch;
+    a = b = c = 10;     // valid
+}
